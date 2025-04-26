8ef3dd17a57cfaf42029a02dfe378848cbacb6e1
Nya Candy <dev@candinya.com>
2025-04-26 11:52:49 +0800

    chore: add \r\n at each stderr to reset terminal start
    13, 10

--- logs.go
+++ logs.go
@@ -3,7 +3,7 @@ package main
 import "os"
 
 func LogError(err error) {
-	_, _ = os.Stderr.WriteString(err.Error())
+	_, _ = os.Stderr.WriteString(err.Error() + "\r\n")
 }
 
 func LogPanic(err error) {
